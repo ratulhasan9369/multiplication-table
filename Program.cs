37a8e6c35e84269cf756623dd43169e72a0387cd
unknown <ratulbdk02@gmail.com>
2024-02-25 22:10:34 +0600

    Initialization done

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+class Multiplication_Table
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Enter your start number: ");
+        int startNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter your end number: ");
+        int endNumber = Convert.ToInt32(Console.ReadLine());
+
+
+        for (int i = startNumber; i <= endNumber; i++)
+        {
+
+            for (int j = 1; j <= 10; j++)
+            {
+                Console.WriteLine($"{i} * {j} = {i * j}");
+            }
+            Console.WriteLine("-----------------");
+        }
+    }
+}
